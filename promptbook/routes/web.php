66a38fb221f2fcba0bf320370dac95d5cc87b6ed
Xander172006 <xanderpoggenklaas@gmail.com>
2024-11-14 10:07:48 +0100

    Livewire implemented correctly

--- promptbook/routes/web.php
+++ promptbook/routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Dashboard;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
+
+Route::get('dashboard', Dashboard::class)
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
+
+
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
