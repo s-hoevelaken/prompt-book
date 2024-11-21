32682457632f0e85128ee6b54c4e762e45b877aa
Xander172006 <xanderpoggenklaas@gmail.com>
2024-11-21 14:24:31 +0100

    view page is finished

--- promptbook/app/Livewire/Viewpage.php
+++ promptbook/app/Livewire/Viewpage.php
@@ -3,13 +3,18 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Title;
 use App\Models\Prompt;
 
 class Viewpage extends Component
 {
-    // find user id
+    use WithPagination;
+
+    public $search = '';
+    public $pagination = 12;
+    public $state = 'upload-date'; // Add a property for the selected filter
     public $user_id;
 
     public function mount()
@@ -17,16 +22,32 @@ class Viewpage extends Component
         $this->user_id = Auth::id();
     }
 
-
     #[Title('View your Prompts')]
     public function render()
     {
-        $prompts = Prompt::where('user_id', $this->user_id)->get()->transform(function ($prompt) {
-            $prompt->description = strip_tags($prompt->description);
-            $prompt->content = strip_tags($prompt->content);
-            return $prompt;
-        });
-    
+        $query = Prompt::where('user_id', $this->user_id)
+            ->where('title', 'like', '%' . $this->search . '%');
+
+        // Apply the filter based on the selected state
+        switch ($this->state) {
+            case 'old-date':
+                $query->orderBy('created_at', 'asc');
+                break;
+            case 'total-likes':
+                $query->orderBy('likes', 'desc');
+                break;
+            default:
+                $query->orderBy('created_at', 'desc');
+                break;
+        }
+
+        $prompts = $query->paginate($this->pagination)
+            ->through(function ($prompt) {
+                $prompt->description = strip_tags($prompt->description);
+                $prompt->content = strip_tags($prompt->content);
+                return $prompt;
+            });
+
         return view('livewire.pages.viewpage', [
             'prompts' => $prompts
         ])->layout('layouts.app');
---
+++ promptbook/database/factories/PromptFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Prompt>
+ */
+class PromptFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            // use user_id of users that exist in the database
+            'user_id' => User::inRandomOrder()->first()->id,
+            'title' => $this->faker->title . ' ' . $this->faker->name,
+            'description' => $this->faker->paragraph,
+            'content' => $this->faker->text,
+            'is_public' => $this->faker->boolean,
+        ];
+    }
+}
